2bbf48dd44144219ade22130d3ab4a5b5ae6bdc9
vsukhin <vsukhin@virtuososoft.com>
2016-06-07 14:31:53 +0300

    Testing outgoing connections

--- index.php
+++ index.php
@@ -15,26 +15,7 @@ running on a
 Try <a href="phpinfo.php">phpinfo()</a>.
 <?php
 
-// Requiring TLS 1.0 or better when using file_get_contents():
-$ctx = stream_context_create([
-    'ssl' => [
-        'crypto_method' => STREAM_CRYPTO_METHOD_TLS_CLIENT,
-    ],
-]);
-$html = file_get_contents('https://google.com/', false, $ctx);
-
-echo $html;
-
-// Requiring TLS 1.1 or 1.2:
-$ctx = stream_context_create([
-    'ssl' => [
-        'crypto_method' => STREAM_CRYPTO_METHOD_TLSv1_1_CLIENT |
-                           STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT,
-    ],
-]);
-$html = file_get_contents('https://facebook.com/', false, $ctx);
-
-echo $html;
+echo file_get_contents('http://173.255.118.47');
 
 ?>
 </body>
